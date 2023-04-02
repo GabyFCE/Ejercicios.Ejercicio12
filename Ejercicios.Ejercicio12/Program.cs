c2f2adbee42fc1ae805f96c2907ce1b75a393221
GabyFCE <duartegabriel2017@gmail.com>
2023-04-02 16:09:31 -0300

    Add project files.

---
+++ Ejercicios.Ejercicio12/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+Console.WriteLine("Ingrese dos frases para iniciar comparación");
+Console.WriteLine("Ingrese frase 1");
+string frasea = Console.ReadLine();
+Console.WriteLine("Ingrese frase 2");
+string fraseb = Console.ReadLine();
+
+if (frasea.CompareTo(fraseb) == 0)
+{
+    Console.WriteLine("Las dos frases son idénticas");
+}
+else
+{
+    Console.WriteLine("Las frases son distintas");
+}
